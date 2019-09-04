0e6f5d9dcf9ef06aa1447b91f888d538a1159616
Elizabeth Le <elizabethlemk@yahoo.com>
2019-09-04 15:38:19 -0400

    about page done

--- src/components/About.js
+++ src/components/About.js
@@ -4,48 +4,61 @@ const About = () => {
   return (
     <div className="about wrapper" data-aos="fade-up">
       <div className="img_wrapper">
-        <img src="https://i.imgur.com/4Y3dLBk.png" alt="profile" />
+        <img
+          src="https://i.imgur.com/4Y3dLBk.png"
+          alt="profile"
+          data-aos="fade-down"
+        />
+        <div className="about_text" data-aos="fade-down">
+          <h2>Background</h2>
+          <p>
+            Born and raised in Orange County, California, I always had a passion
+            for the arts since childhood. In college, I channeled my creative
+            spirit into a more practical application: fashion design.
+            <br /> As much as I adored fashion, I found a passion and love for
+            programming. The process in software engineering is extremely
+            similar to garment creation in fashion!
+          </p>
+
+          <h2>Skills</h2>
+          <p>
+            React, Redux, JavaScript, Ruby on Rails, PHP, CMS (Wordpress,
+            Shopify)
+            <br />
+            CSS, HTML, Illustrator, Photoshop, PostgreSQL, AWS S3
+          </p>
+        </div>
       </div>
       <div className="about_wrapper">
-        <h1 className="about-header">Background</h1>
-        <p>
-          Born and raised in Orange County, California, I always had a passion
-          for the arts since childhood. In college, I channeled my creative
-          spirit into a more practical application: fashion design.
-          <br /> As much as I adored fashion, I found a passion and love for
-          programming. The process in software engineering is extremely similar
-          to garment creation in fashion!
-        </p>
-
-        <h1 className="about-header">Skills</h1>
-        <hr />
-        <p>
-          React, Redux, JavaScript, Ruby on Rails, PHP, CMS (Wordpress, Shopify)
-          <br />
-          CSS, HTML, Illustrator, Photoshop, PostgreSQL, AWS S3
-        </p>
-
-        <h1 className="about-header">Education</h1>
-        <hr />
-        <ul>
-          <li>
-            <ul>
-              <h5 className="school">Flatiron School </h5>
-              January - April 2019
-              <li>Brooklyn, NY</li>
-              <li>Immersive 15 weeks program full stack development</li>
-            </ul>
-          </li>
-          <br />
-          <li>
-            <ul>
-              <h5 className="school">California State University Long Beach</h5>
-              2015-2018
-              <li>Long Beach, CA</li>
-              <li>B.A. in Fashion Design</li>
-            </ul>
-          </li>
-        </ul>
+        <div className="about_text" data-aos="fade-down">
+          <h2>Education</h2>
+          <ul>
+            <li>
+              <ul>
+                <h3>Flatiron School </h3>
+                January - April 2019
+                <li>Brooklyn, NY</li>
+                <li>Immersive 15 weeks program full stack development</li>
+              </ul>
+            </li>
+            <br />
+            <li>
+              <ul>
+                <h3>California State University Long Beach</h3>
+                2015-2018
+                <li>Long Beach, CA</li>
+                <li>B.A. in Fashion Design</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img
+            src="https://i.imgur.com/o7pDIny.png"
+            alt="profile2"
+            data-aos="fade-down"
+          />
+        </div>
       </div>
     </div>
   );
