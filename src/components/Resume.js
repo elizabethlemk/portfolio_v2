75da250da59ebc2dd176b8104171cf55f408cd65
Elizabeth Le <elizabethlemk@yahoo.com>
2019-09-10 10:18:02 -0400

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import resume from "../assets/resume.pdf";
 const Resume = () => {
   return (
     <div className="resume wrapper" data-aos="fade-up">
-      <iframe src={resume} scrolling="yes" title="resume" />
+      <object type="application/pdf" data={resume} title="resume" />
     </div>
   );
 };
