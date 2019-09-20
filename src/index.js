06bbad5b5b0a61a98166582901665de5e50182ed
Elizabeth Le <elizabethlemk@yahoo.com>
2019-09-20 11:19:15 -0400

    crabs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+console.log("🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀");
+console.log("🦀🦀🦀  Welcome to 🦀🦀🦀");
+console.log("🦀🦀🦀🦀the crab 🦀🦀🦀🦀");
+console.log("🦀🦀🦀  P A R T Y  🦀🦀🦀");
+console.log("🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀");
 ReactDOM.render(
   <BrowserRouter>
     <App />
